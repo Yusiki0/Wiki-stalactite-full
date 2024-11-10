8927eb6a2d322f8e8cc05965a257647c62e10f57
Yusiki <154873648+Yusiki0@users.noreply.github.com>
2024-11-10 17:58:56 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    pseudo: String,
+    message: String,
+    timestamp: { type: Date, default: Date.now },
+});
+
+const Message = mongoose.model('Message', messageSchema);
