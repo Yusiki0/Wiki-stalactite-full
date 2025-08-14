9a2cbed5141e6cb68fd12ec65049fdf33580a4d1
Yusiki <154873648+Yusiki0@users.noreply.github.com>
2025-08-14 14:58:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-vimport { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
